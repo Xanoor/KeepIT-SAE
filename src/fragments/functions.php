c9bb854470dbf0f150ef6cdad364655d0152bd93
Xanoor <t.peyronnet@outlook.fr>
2025-11-26 02:02:38 +0100

    Create importTableBuilder function (load csv).

--- src/fragments/functions.php
+++ src/fragments/functions.php
@@ -1,4 +1,35 @@
 <?php 
 
+/**
+ * Create an HTML table from a CSV file.
+ *
+ * @param resource $file File pointer to the CSV file.
+ * @param int $limit Maximum number of rows to read (default 100).
+ * @return string HTML table.
+ */
+function importTableBuilder($file, $limit = 100) {
+    $result = fgetcsv($file);
+
+    // Header
+    $html = "<table><tr>";
+    foreach ($result as $value) {
+        $html .= "<th>" . htmlspecialchars($value) . "</th>";
+    }
+    $html .= "</tr>";
+
+    $counter = 0;
+    // Body
+    while (($result = fgetcsv($file)) && $counter < $limit) {
+        $counter++;
+        $html .= "<tr>";
+        foreach ($result as $value) {
+            $html .= "<td>" . htmlspecialchars($value) . "</td>";
+        }
+        $html .= "</tr>";
+    }
+    $html .= "</table>";
+
+    return $html;
+}
 
 ?>
