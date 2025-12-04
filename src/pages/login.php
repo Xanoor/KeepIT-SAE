1e4feb9466b28e79e0051347491cc9ab159b70e6
ItsTheSky <livegogo.16334@gmail.com>
2025-12-04 16:56:08 +0100

    :art: login page code cleanup

--- src/pages/login.php
+++ src/pages/login.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html> 
-<html>
-<!-- <?php include 'header.php'; ?> -->
-<?php include '../fragments/functions.php'; ?>
 <?php
-
+include '../fragments/functions.php';
 
 session_start();
 
@@ -11,12 +7,15 @@ if (isset($_SESSION['login'])) {
     header("Location: index.php");
     exit();
 }
-
-
 ?>
+<!DOCTYPE html>
+<html>
+
+<!-- <?php include 'header.php'; ?> -->
 
 <body>
     <main>
+        <?php echo icon('accessibility'); ?>
         <form action='../actions/login_action.php' method='POST'>
             <input type='text' name="login_email" placeholder='Email or login'/>
             <input type='password' name="password" placeholder='Password'/>
