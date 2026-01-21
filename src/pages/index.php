87184ddf159b6a146603941271dcba14f0be44d6
ItsTheSky <livegogo.16334@gmail.com>
2026-01-21 13:23:06 +0100

    feat: update user session display and logout redirection

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,5 +9,8 @@ if (!isset($_SESSION['login'])) {
 
 echo "Infos de l'utilisateur connecté :<br>";
 print_r($_SESSION);
+echo "<br><br>";
 
 echo "Dernière connexion : " . date("d/m/Y H:i:s", $_SESSION['last_activity']);
+echo "<br><br>";
+echo "<a href='../actions/logout_action.php'>Se déconnecter</a>";
