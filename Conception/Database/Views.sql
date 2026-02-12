97f064f9eee9c979a3bb8c46a24c65e947ad07cd
ZPitor99 <gabriel.chifflet7845@outlook.fr>
2026-02-12 23:34:25 +0100

    View to export computers - monitors

--- Conception/Database/Views.sql
+++ Conception/Database/Views.sql
@@ -8,12 +8,13 @@ CREATE OR REPLACE VIEW vw_monitor_by_computer AS
         m.serial_number AS serial_monitor,
         m.connector_name AS connector,
         c.serial_number AS serial_computer,
+        c.name AS computer_name,
         c.type_name AS computer_type
     FROM
         monitor m,
         computer c
     WHERE
-        m.attached_to_serial = c.serial_number;
+        m.attached_to_computer = c.name;
 
 -- ACTION/MONTH
 CREATE OR REPLACE VIEW vw_action_on_device_by_month AS
@@ -44,13 +45,13 @@ CREATE OR REPLACE VIEW vw_month_activity AS
         nb_utilisateurs,
         -- Classement des champs les plus modifiés par mois
         DENSE_RANK() OVER (PARTITION BY mois ORDER BY nb_modifications DESC) AS month_rank,
-        -- Classement global
+            -- Classement global
         RANK() OVER (ORDER BY nb_modifications DESC) AS global_rank,
-        -- Percentile dans le mois
+            -- Percentile dans le mois
         PERCENT_RANK() OVER (PARTITION BY mois ORDER BY nb_modifications DESC) AS percentile_mois,
-        -- Évolution vs mois précédent
+            -- Évolution vs mois précédent
         nb_modifications - LAG(nb_modifications) OVER (PARTITION BY field_updated ORDER BY mois) AS evolution_vs_last_month,
-        -- Évolution en pourcentage
+            -- Évolution en pourcentage
         ROUND(
                 (nb_modifications - LAG(nb_modifications) OVER (PARTITION BY field_updated ORDER BY mois))
                     / NULLIF(LAG(nb_modifications) OVER (PARTITION BY field_updated ORDER BY mois), 0) * 100,
@@ -58,3 +59,54 @@ CREATE OR REPLACE VIEW vw_month_activity AS
         ) AS evolution_pct
     FROM monthly_field_activity
     ORDER BY mois DESC, month_rank;
+
+-- VIEW EXPORT DATA
+CREATE OR REPLACE VIEW vw_export_computer AS
+       SELECT
+           dv.serial_number AS serial_number,
+           dv.model AS model,
+           dv.created_at AS created_at,
+           dv.updated_at AS last_update,
+           dv.state AS state,
+           c.name AS name,
+           c.manufacturer_name as manufacturer,
+           c.cpu AS cpu,
+           c.ram_mb AS ram_mb,
+           c.disk_gb AS disk_gb,
+           c.os_name AS os,
+           c.domain AS domain,
+           c.location AS location,
+           c.building AS building,
+           c.room AS room,
+           c.mac_address AS macaddr,
+           c.purchase_date AS purchase_date,
+           c.warranty_end AS warranty_end
+       FROM
+           computer c,
+           devices dv
+       WHERE
+                c.serial_number = dv.serial_number
+            AND dv.device_type LIKE 'Computer'
+       ORDER BY
+           dv.serial_number;
+
+CREATE OR REPLACE VIEW vw_export_monitor AS
+       SELECT
+           dv.serial_number AS serial_number,
+           dv.model AS model,
+           dv.created_at AS created_at,
+           dv.updated_at AS last_update,
+           dv.state AS state,
+           m.manufacturer_name AS manufacturer,
+           m.size_inch AS size_inch,
+           m.resolution AS resolution,
+           m.connector_name AS connector,
+           m.attached_to_computer AS attached_to
+       FROM
+           monitor m,
+           devices dv
+       WHERE
+                m.serial_number = dv.serial_number
+            AND dv.device_type LIKE 'Monitor'
+       ORDER BY
+           dv.serial_number;
