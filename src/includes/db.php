c244ef278662dcf46b4b6c401ceb67a21d556c1f
= <t.peyronnet@outlook.fr>
2026-02-13 10:00:43 +0100

    Configure db host values for raspberry pi

--- src/includes/db.php
+++ src/includes/db.php
@@ -6,7 +6,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 function create_connection() {
     if (!isset($GLOBALS['connect'])) {
-        $GLOBALS['connect'] = mysqli_connect("localhost", "root", "", "keepit");
+        $GLOBALS['connect'] = mysqli_connect("localhost", "system", "!sae2025!", "keepit");
         mysqli_set_charset($GLOBALS['connect'], "utf8");
 
         // Check connection
