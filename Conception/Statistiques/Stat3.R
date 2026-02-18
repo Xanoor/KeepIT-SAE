fa184407159ec54a70ec1a6c8ccb49142c60c541
Nourane <athounourane@gmail.com>
2026-02-18 21:27:48 +0100

    Add scripts to generate stats

---
+++ Conception/Statistiques/Stat3.R
@@ -0,0 +1,34 @@
+moyennes_connections <- function(fichier, nombre_paliers) {
+  
+  donnees = read.csv(fichier, sep = ",")
+  
+  stats_moyennes = aggregate(duration_seconds ~ login, 
+                              data = donnees, 
+                              FUN = mean)
+  stats_moyennes$moyenne_min = stats_moyennes$duration_seconds / 60
+  
+  val_min = min(stats_moyennes$moyenne_min)
+  val_max = max(stats_moyennes$moyenne_min)
+  
+  pas = (val_max - val_min) / nombre_paliers
+  
+  paliers = c()
+  valeurs = c()
+  
+  for (i in 1:nombre_paliers) {
+    borne_basse = val_min + (i - 1) * pas
+    borne_haute = val_min + i * pas
+    effectif <- sum(stats_moyennes$moyenne_min >= borne_basse & stats_moyennes$moyenne_min <= borne_haute)
+    paliers[i] = round(borne_basse, 0)
+    valeurs[i] = effectif
+  }
+  
+  affichage = data.frame(
+    Palier = paliers,
+    Effectif = valeurs
+  )
+  
+  return(affichage)
+}
+
+moyennes_connections("connections.csv", 10)
