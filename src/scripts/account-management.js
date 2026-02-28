1a11e4253f03eddb96b3bf8fc71043151425cae3
Daniel Rodrigues Amorim <105808962+Pintass@users.noreply.github.com>
2026-02-28 23:37:45 +0100

    [+] Include "Account Management" static page.

---
+++ src/scripts/account-management.js
@@ -0,0 +1,11 @@
+document.querySelector('.eye-icon').addEventListener('click', function () {
+    const passwordInput = document.getElementById('password');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        this.classList.add('visible');
+    } else {
+        passwordInput.type = 'password';
+        this.classList.remove('visible');
+    }
+});
