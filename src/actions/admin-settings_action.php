93f11e14ccdb2adb8482d9f7474e2041fae9862e
Xanoor <t.peyronnet@outlook.fr>
2026-04-30 23:26:18 +0200

    Add backend logic for creating new variables action.

--- src/actions/admin-settings_action.php
+++ src/actions/admin-settings_action.php
@@ -1,5 +1,4 @@
 <?php
-    // TODO: create admin settings new variable actions
     session_start();
 
     // Only the web admin can access this action
@@ -9,6 +8,7 @@
     }
 
     include_once("../includes/functions.php");
+    global $connect; 
 
     // Delete var button
     if (isset($_POST["DELETE_VAR"], $_POST["var-items"], $_POST["var-column"], $_POST["var-name"])) {
@@ -59,6 +59,71 @@
             $_SESSION['notification_color'] = "red";
         }
 
+        header("Location: ../pages/admin-settings.php");
+        exit();
+    } else if (isset($_POST["table-select"], $_POST["var-value"], $_POST["CREATE_VAR"])) {
+        $tableName = $_POST["table-select"];
+        $varValue = $_POST["var-value"];
+        
+        if (!tableExists($connect, $tableName)) {
+            $_SESSION['notification'] = "La table ".$tableName." n'est pas présente dans la base de donnée.";
+            $_SESSION['notification_color'] = "red";
+            header("Location: ../pages/admin-settings.php");
+            exit();
+        }
+
+        $columnName = "";
+           
+        // Depending on the table selected, execute a customized SELECT query. 
+        // For example: if a table requires multiple values, then create a new case.
+        switch ($tableName) {
+            case 'connector':
+            case 'operating_system':
+            case 'manufacturer':
+                $columnName = "name";
+                break;
+
+            case 'locations':
+                $columnName = "location";
+                break;
+
+            default:
+                $_SESSION['notification'] = "L'action liée a cette table n'a pas été spécifiée dans le code.";
+                $_SESSION['notification_color'] = "red";
+                header("Location: ../pages/admin-settings.php");
+                exit();
+        }
+        // if the query is the default one (only one column to edit), use the query:
+
+        if (!columnsExists($connect, $tableName, [$columnName])) {
+            $_SESSION['notification'] = "La colonne pour la table ".$tableName." est introuvable.".$columnName;
+            $_SESSION['notification_color'] = "red";
+            header("Location: ../pages/admin-settings.php");
+            exit();
+        }
+
+        if (checkDatabaseExistence($connect, $tableName, $columnName, $varValue)) {
+            $_SESSION['notification'] = "La valeur ".$varValue." est déja présente dans la table.";
+            $_SESSION['notification_color'] = "red";
+            header("Location: ../pages/admin-settings.php");
+            exit();
+        }
+
+        $add_query = "INSERT INTO $tableName ($columnName) VALUES (?)";
+        $add_stmt = mysqli_prepare($connect, $add_query);
+        mysqli_stmt_bind_param($add_stmt, "s", 
+            $varValue
+        );
+        
+        if (!mysqli_stmt_execute($add_stmt)) {
+            $_SESSION['notification'] = "Une erreur est arrivée pendant l'insertion de la valeur.";
+            $_SESSION['notification_color'] = "red";
+            header("Location: ../pages/admin-settings.php");
+            exit();
+        }
+
+        $_SESSION['notification'] = "La valeur ".$varValue." à été ajouté a la table ".$tableName;
+        $_SESSION['notification_color'] = "#5CE65C";
         header("Location: ../pages/admin-settings.php");
         exit();
     }
